d4da2fb6f1d68723aab8012137bdeeb15b9bf2a0
mio.math <noabusiness@163.com>
2024-11-23 22:01:07 +0100

    Updates

---
+++ client/assets/config-BKfNIIMc.js
@@ -0,0 +1,5 @@
+const o="Mio Chibana",t="Designer",s=["Developer","Prototyper","Animator","Illustrator","Modder"],i="https://miodev.us.kg",n="miodev.us.kg",e="nonoko_4649",r="MioGlobalDev",c="https://github.com/HamishMW/portfolio",l=`__  __  __
+\\ \\ \\ \\ \\∕
+ \\ \\∕\\ \\
+  \\∕  \\∕
+`,a={name:o,role:t,disciplines:s,url:i,bluesky:n,Twitter:e,github:r,repo:c,ascii:l};export{a as c};
